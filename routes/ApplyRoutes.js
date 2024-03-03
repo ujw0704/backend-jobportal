5082a6a7b251c11aaffe333a52fe0c56f40853c0
ujw0704 <ujwalsharma571@gmail.com>
2024-03-03 20:21:38 +0530

    fewchanges

--- routes/ApplyRoutes.js
+++ routes/ApplyRoutes.js
@@ -1,27 +1,4 @@
-// import express from "express";
-// import ApplyJobs from "../controller/ApplyController.js";
-// import multer from 'multer';
-// import path from 'path'
 
-// const router = express.Router()
-//  //multer configure
-
-//  const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, 'uploads/'); 
-//     },
-//     filename: function (req, file, cb) {
-//       const ext = path.extname(file.originalname);  
-//       const filename = Date.now() + "-" + file.originalname; 
-//       cb(null, filename);
-//     },
-//   });
-  
-//   const upload = multer({ storage: storage });
-
-// router.post("/applyJobs", upload.single('resume'), ApplyJobs)
-
-// export default router
 import express from "express";
 import ApplyJobs from "../controller/ApplyController.js";
 import multer from 'multer';
